91ef43754ab8e1d5c04faf857acc48827a233ae5
zak aden <faf>
2021-09-05 15:50:49 +0100

    styling fixes

--- js/dist/styles.dev.js
+++ js/dist/styles.dev.js
@@ -2,8 +2,16 @@
 
 var tab = document.querySelectorAll('.tab');
 tab.forEach(function (button) {
-  button.addEventListener('click', function (e) {
-    button.classList.add('active');
+  button.addEventListener('click', function (event) {
+    // document.querySelector('.tab.active')?.classList.remove('active');
+    // button.classList.add('active')
+    // console.log(event.currentTarget); // show what element was clicked in the console
+    // console.log(event.currentTarget.dataset); // get .dataset Object key from HTML element
+    // document.querySelector('.tab.active')?.classList.remove('active'); // optional chaining
+    button.classList.contains('active') ? button.classList.remove('active') : button.classList.add('active'); // event.currentTarget.classList.add('active')
+
+    document.querySelector('.project_one.show').classList.remove('show'); // button.classList.contains('active')? button.classList.remove('active'): button.classList.add('active')
+
     console.log(button);
   });
 });
--- js/styles.js
+++ js/styles.js
@@ -2,8 +2,24 @@ const tab = document.querySelectorAll('.tab')
 
 tab.forEach((button) => {
     
-    button.addEventListener('click', (e) => {
-        button.classList.add('active')
+    button.addEventListener('click', (event) => {
+        
+        // document.querySelector('.tab.active')?.classList.remove('active');
+
+        // button.classList.add('active')
+        // console.log(event.currentTarget); // show what element was clicked in the console
+    // console.log(event.currentTarget.dataset); // get .dataset Object key from HTML element
+
+    // document.querySelector('.tab.active')?.classList.remove('active'); // optional chaining
+
+        button.classList.contains('active') ? button.classList.remove('active'): button.classList.add('active')
+
+        // event.currentTarget.classList.add('active')
+
+        document.querySelector('.project_one.show').classList.remove('show');
+    
+
+        // button.classList.contains('active')? button.classList.remove('active'): button.classList.add('active')
         console.log(button);
     } )
 })
